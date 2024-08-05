974fb41ff364ad22c290e771aee5f15cbe3c9e90
morris19-mw <machariamorris19@gmail.com>
2024-08-05 12:19:56 +0300

    changes update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('MORRIS MACHARIA learning code deliverly pipeline using AWS console!'));
+app.get('/', (req, res) => res.send('MORRIS MACHARIA learning to process code delivery in aws console self sponsored!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
